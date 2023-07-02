edd614af8a0ba148d57b8f64507a6adccc8b5c31
Mr Pumpkin <129291449+FriendlyPumpkin@users.noreply.github.com>
2023-07-03 02:58:04 +0800

    Update colors.py

--- design/colors.py
+++ design/colors.py
@@ -1,1 +1,6 @@
+from colorama import Fore, Style
+import random
 
+all_color= [Style.BRIGHT+Fore.RED,Style.BRIGHT+Fore.CYAN,Style.BRIGHT+Fore.LIGHTCYAN_EX, Style.BRIGHT+Fore.LIGHTBLUE_EX, Style.BRIGHT+Fore.LIGHTCYAN_EX,Style.BRIGHT+Fore.LIGHTMAGENTA_EX,Style.BRIGHT+Fore.LIGHTYELLOW_EX]
+
+colr = random.choice(all_color)
