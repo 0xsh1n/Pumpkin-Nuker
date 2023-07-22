ca88d5fe0b8d239c0e52e7d030aced4a5b42239e
pumpkin <129291449+kinxyz@users.noreply.github.com>
2023-07-22 13:45:11 +0800

    Update banner.py

--- design/banner.py
+++ design/banner.py
@@ -1,7 +1,16 @@
-from design.colors import *
+from colorama import Fore, Style
+import random
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-logo = f'''
+all_color = [Style.BRIGHT + Fore.RED, Style.BRIGHT + Fore.LIGHTCYAN_EX]
+chosen_color = random.choice(all_color)
+
+logo = '''
                                                                       
  ██▓███   █    ██  ███▄ ▄███▓ ██▓███   ██ ▄█▀ ██▓ ███▄    █ 
 ▓██░  ██▒ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒ ██▄█▒ ▓██▒ ██ ▀█   █ 
@@ -13,9 +22,20 @@ logo = f'''
 ░░        ░░░ ░ ░ ░      ░   ░░       ░ ░░ ░  ▒ ░   ░   ░ ░ 
             ░            ░            ░  ░    ░           ░ 
                                                             
-                                                       \n Made by _notpumpkin'''
-                                                       
-def banner():
-    print(colr + logo)
-                                                       
+                                                       '''
+
+def banner(color):
+    print(color + logo)
+
+def countdown():
+  print(Style.BRIGHT + Fore.LIGHTYELLOW_EX, "\n\n\t\tPreparing Nuker...", Style.RESET_ALL)
+  
+def countdown2():
+  print(Style.BRIGHT + Fore.LIGHTYELLOW_EX, "\n\n\t\tDone...", Style.RESET_ALL)
+def text():
+
+    print("\n", chosen_color, "  " * 6, f"Logged in as {bot.user}", "  " * 6 + Style.RESET_ALL)
+    print("\n", chosen_color, "  " * 7, "Made by _notpumpkin", "  " * 6 + Style.RESET_ALL)
+    print("\n", chosen_color, "  " * 8, "!cmd for help", "  " * 6 + Style.RESET_ALL)
+
     
