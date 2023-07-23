7a68906a97c3b945823a5f25261231418093ba9b
pumpkin <129291449+kinxyz@users.noreply.github.com>
2023-07-24 00:28:24 +0800

    Update banner.py

--- design/banner.py
+++ design/banner.py
@@ -12,16 +12,16 @@ chosen_color = random.choice(all_color)
 
 logo = '''
                                                                       
- ██▓███   █    ██  ███▄ ▄███▓ ██▓███   ██ ▄█▀ ██▓ ███▄    █ 
-▓██░  ██▒ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒ ██▄█▒ ▓██▒ ██ ▀█   █ 
-▓██░ ██▓▒▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▓███▄░ ▒██▒▓██  ▀█ ██▒
-▒██▄█▓▒ ▒▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▓██ █▄ ░██░▓██▒  ▐▌██▒
-▒██▒ ░  ░▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░▒██▒ █▄░██░▒██░   ▓██░
-▒▓▒░ ░  ░░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░▒ ▒▒ ▓▒░▓  ░ ▒░   ▒ ▒ 
-░▒ ░     ░░▒░ ░ ░ ░  ░      ░░▒ ░     ░ ░▒ ▒░ ▒ ░░ ░░   ░ ▒░
-░░        ░░░ ░ ░ ░      ░   ░░       ░ ░░ ░  ▒ ░   ░   ░ ░ 
-            ░            ░            ░  ░    ░           ░ 
-                                                            
+\t ██▓███   █    ██  ███▄ ▄███▓ ██▓███   ██ ▄█▀ ██▓ ███▄    █ 
+\t▓██░  ██▒ ██  ▓██▒▓██▒▀█▀ ██▒▓██░  ██▒ ██▄█▒ ▓██▒ ██ ▀█   █ 
+\t▓██░ ██▓▒▓██  ▒██░▓██    ▓██░▓██░ ██▓▒▓███▄░ ▒██▒▓██  ▀█ ██▒
+\t▒██▄█▓▒ ▒▓▓█  ░██░▒██    ▒██ ▒██▄█▓▒ ▒▓██ █▄ ░██░▓██▒  ▐▌██▒
+\t▒██▒ ░  ░▒▒█████▓ ▒██▒   ░██▒▒██▒ ░  ░▒██▒ █▄░██░▒██░   ▓██░
+\t▒▓▒░ ░  ░░▒▓▒ ▒ ▒ ░ ▒░   ░  ░▒▓▒░ ░  ░▒ ▒▒ ▓▒░▓  ░ ▒░   ▒ ▒ 
+\t░▒ ░     ░░▒░ ░ ░ ░  ░      ░░▒ ░     ░ ░▒ ▒░ ▒ ░░ ░░   ░ ▒░
+\t░░        ░░░ ░ ░ ░      ░   ░░       ░ ░░ ░  ▒ ░   ░   ░ ░ 
+\t            ░            ░            ░  ░    ░           ░ 
+\t                                                            
                                                        '''
 
 def banner(color):
@@ -34,8 +34,8 @@ def countdown2():
   print(Style.BRIGHT + Fore.LIGHTYELLOW_EX, "\n\n\t\tDone...", Style.RESET_ALL)
 def text():
 
-    print("\n", chosen_color, "  " * 6, f"Logged in as {bot.user}", "  " * 6 + Style.RESET_ALL)
-    print("\n", chosen_color, "  " * 7, "Made by _notpumpkin", "  " * 6 + Style.RESET_ALL)
-    print("\n", chosen_color, "  " * 8, "!cmd for help", "  " * 6 + Style.RESET_ALL)
+    print("\n\t", chosen_color, "  " * 7, f"Logged in as {bot.user}", "  " * 6 + Style.RESET_ALL)
+    print("\n\t", chosen_color, "  " * 8, "Made by _notpumpkin", "  " * 6 + Style.RESET_ALL)
+    print("\n\t", chosen_color, "  " * 9, "!cmd for help", "  " * 6 + Style.RESET_ALL)
 
     
